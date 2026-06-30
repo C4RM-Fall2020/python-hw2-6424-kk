027531341054da92cc28afefe0da6d120c4e3d47
6424-kk <yk3175@columbia.edu>
2026-06-29 21:58:29 -0400

    Refactor getBondDuration to calculate bond duration

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,19 @@
 import numpy as np
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    periods = np.arange(1, m * ppy + 1)
+
+    coupon = face * couponRate / ppy
+
+    cashflows = np.full(m * ppy, coupon)
+    cashflows[-1] = cashflows[-1] + face
+
+    pv = (1 + y / ppy) ** (-periods)
+
+    pvcf = cashflows * pv
+
+    weighted_pvcf = (periods / ppy) * pvcf
+
+    bondDuration = np.sum(weighted_pvcf) / np.sum(pvcf)
+
+    return(bondDuration)
